5fdd10d6dbbe8b95db4db738185171ee6cc67b3d
Eliot McIntire <eliotmcintire@gmail.com>
2019-02-28 16:25:02 -0800

    updates to SpiderWeb

--- SpiderWeb.R
+++ SpiderWeb.R
@@ -4,15 +4,22 @@ library(fmsb)
 #set.seed(99)
 #a <- lapply(1:3, function(x) sample(0:100, 5))
 
-data=as.data.frame(matrix( sample( 30:80 , 15 , replace=F) , ncol=5))
-colnames(data)=c("Carbon sequestration" , "Old growth retention" , 
-                 "MPB population declining" , 
-                 "Fire proofing within 100km communities" , 
-                 "Caribou recovery (lambda > 1.02)" )
-rownames(data)=paste("Management scenario" , letters[1:3] , sep=" ")
+objectives <- c("Carbon sequestration" , 
+                "Old growth retention" , 
+                "MPB population\ndeclining" , 
+                "Fire proofing within\n100km communities" , 
+                "Caribou recovery\n(lambda > 1.02)",
+                "Timber supply\nobjectives reached",
+                "Biodiversity")
+numScenarios <- 3
+numObjectives <- length(objectives)
+data=as.data.frame(matrix( sample( 30:80 , numScenarios * numObjectives, 
+                                   replace=F) , ncol=numObjectives))
+colnames(data)=objectives
+rownames(data)=paste(letters[seq(numScenarios)] , sep=" ")
 
 # To use the fmsb package, I have to add 2 lines to the dataframe: the max and min of each topic to show on the plot!
-data=rbind(rep(100,5) , rep(0,5) , data)
+data=rbind(rep(100, numObjectives) , rep(0, numObjectives) , data)
 
 
 
@@ -20,13 +27,15 @@ data=rbind(rep(100,5) , rep(0,5) , data)
 
 #==================
 # Plot 1: Default radar chart proposed by the library:
-radarchart(data)
+#radarchart(data)
 
 
 #==================
 # Plot 2: Same plot with custom features
 colors_border=c( rgb(0.2,0.5,0.5,0.9), rgb(0.8,0.2,0.5,0.9) , rgb(0.7,0.5,0.1,0.9) )
 colors_in=c( rgb(0.2,0.5,0.5,0.4), rgb(0.8,0.2,0.5,0.4) , rgb(0.7,0.5,0.1,0.4) )
+png(filename = "~/Google Drive/McIntire-lab/figures/Tradeoff management values.png", 
+    width = 800)
 radarchart( data  , axistype=2,  
             #custom polygon
             pcol=colors_border , pfcol=colors_in , plwd=4 , plty=1,
@@ -35,6 +44,8 @@ radarchart( data  , axistype=2,
             #custom labels
             vlcex=0.8 
 )
-legend(x=0.7, y=1, legend = rownames(data[-c(1,2),]), bty = "n", pch=20 , 
-       col=colors_in , text.col = "grey", cex=1.2, pt.cex=3)
-
+mtext(outer = TRUE, side = 3, text = paste("Example tradeoff analysis using integrated\n",
+                                           "modular tools, in SpaDES"), line = -3)
+legend(x=1, y=1.3, legend = rownames(data[-c(1,2),]), bty = "n", pch=20 , 
+       col=colors_in , text.col = "grey", cex=1.2, pt.cex=3, title = "Management scenario")
+dev.off()
